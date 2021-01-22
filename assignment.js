d0366a3922c7242907d5c6b617155a75c0eef6e9
Mehdi39 <jahid4k@gmail.com>
2021-01-22 21:28:25 +0600

    final commit

--- assignment.js
+++ assignment.js
@@ -2,8 +2,8 @@
 // function for converting kilometer to meter.
 function kilometerToMeter(kilometers) {
     if (kilometers <= 0) {
-        var lol = "Be Positive, plz"
-        return lol;
+        var errorMS = "Be Positive, plz"
+        return errorMS;
     } else {
         // as we know 1000 meter = 1 kilometer.
         var meter = 1000;
@@ -12,56 +12,45 @@ function kilometerToMeter(kilometers) {
     }
 }
 
-var lol = kilometerToMeter(1);
-console.log(lol);
-
-
-
-
-
-// // function for converting kilometer to meter.
-// function kilometerToMeter(kilometers) {
-    
-//     // as we know 1000 meter = 1 kilometer.
-//     var meter = 1000;
-//     var result = kilometers * meter;
-
-//     return result;
-// }
-
-// var lol = kilometerToMeter(5);
-// console.log(lol);
-
 //  function for calculating watch/mobile/laptop shopping budget
 function budgetCalculator(watch, mobile, laptop) {
+    if (watch <= 0 || mobile <= 0 || laptop <= 0) {
+        var errorMS = "Be Positive, plz"
+        return errorMS;
+    } else {
+        //  price for per watch = $50
+        var watchBudget = 50 * watch;
 
-    //  price for per watch = $50
-    var watchBudget = 50 * watch;
+        //  price for per mobile  = $100
+        var mobileBudget = 100 * mobile;
 
-    //  price for per mobile  = $100
-    var mobileBudget = 100 * mobile;
+        //  price for per laptop = $500
+        var laptopBudget = 500 * laptop;
 
-    //  price for per laptop = $500
-    var laptopBudget = 500 * laptop;
+        var totalBudget = watchBudget + mobileBudget + laptopBudget;
 
-    var totalBudget = watchBudget + mobileBudget + laptopBudget;
-
-    return totalBudget;
+        return totalBudget;
+    }
 }
 
 // function for calculating hotelCost. first 10 days are 100 per night. after 10 days per night costs 80. 
 function hotelCost(days) {
 
-    // checking if customer/client staying 10 days or not.
-    if (days <= 10) {
-        totalCost = days * 100;
-        return totalCost;
-    } else { // code runs if customer/client stay more than 10 days.
-        var baseCost = 1000;
-        var afterDiscountCost = (days - 10) * 80;
-        var totalCost = baseCost + afterDiscountCost;
+    if (days <= 0) {
+        var errorMS = "Be Positive, plz"
+        return errorMS;
+    } else {
+        // checking if customer/client staying 10 days or not.
+        if (days <= 10) {
+            totalCost = days * 100;
+            return totalCost;
+        } else { // code runs if customer/client stay more than 10 days.
+            var baseCost = 1000;
+            var afterDiscountCost = (days - 10) * 80;
+            var totalCost = baseCost + afterDiscountCost;
 
-        return totalCost;
+            return totalCost;
+        }
     }
 }
 
